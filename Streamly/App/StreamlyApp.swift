a2360836122e827954c0d2cc7a4a90f92e7c27bf
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-10-18 00:18:11 -0300

    chore: add .gitignore

--- Streamly/App/StreamlyApp.swift
+++ Streamly/App/StreamlyApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct StreamlyApp: App {
+    init() {
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text("StreamlyApp")
         }
     }
 }
