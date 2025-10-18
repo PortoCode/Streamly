3e323c64037c4ada97e0d4619c9fa5d4553d09b1
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-10-17 23:51:17 -0300

    Initial Commit

---
+++ Streamly/StreamlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamlyApp.swift
+//  Streamly
+//
+//  Created by Rodrigo Porto on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StreamlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
