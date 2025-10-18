139e1a2daa7592515c2c17a2f43ba008fdbc376b
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-10-18 15:35:15 -0300

    feat: add `VideoRowView`

---
+++ Streamly/Presentation/Views/VideoRowView.swift
@@ -0,0 +1,46 @@
+//
+//  VideoRowView.swift
+//  Streamly
+//
+//  Created by Rodrigo Porto on 18/10/25.
+//
+
+import SwiftUI
+
+struct VideoRowView: View {
+    let video: Video
+    let onTap: () -> Void
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            AsyncImage(url: video.image) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                        .frame(width: 120, height: 70)
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 70)
+                        .clipped()
+                case .failure:
+                    Color.gray
+                        .frame(width: 120, height: 70)
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            
+            VStack(alignment: .leading) {
+                Text(video.user?.name ?? "Unknown")
+                    .font(.headline)
+                Text("\(video.duration ?? 0) sec")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .padding(.vertical, 8)
+        .onTapGesture(perform: onTap)
+    }
+}
