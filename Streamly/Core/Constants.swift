9bd6b8c2807ab0127f649d08777cd26b1f766a10
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-10-25 00:34:58 -0300

    feat: add downloads folder name to Constants enum

--- Streamly/Core/Constants.swift
+++ Streamly/Core/Constants.swift
@@ -10,4 +10,5 @@ import Foundation
 enum Constants {
     static let pexelsApiKey = "PEXELS_API_KEY"
     static let pexelsBaseUrl = "https://api.pexels.com/videos/"
+    static let downloadsFolderName = "StreamlyDownloads"
 }
