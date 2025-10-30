ff6a1f558116bf7bffe0d7d3470033be413b52ca
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-10-29 22:45:54 -0300

    feat: handle local, remote, and unavailable video in makeUIViewController

--- Streamly/Player/VideoPlayerContainer.swift
+++ Streamly/Player/VideoPlayerContainer.swift
@@ -12,11 +12,27 @@ struct VideoPlayerContainer: UIViewControllerRepresentable {
     let video: Video
     
     func makeUIViewController(context: Context) -> UIViewController {
-        guard let link = video.videoFiles?.sorted(by: { $0.quality ?? "" > $1.quality ?? "" }).first?.link else {
-            return UIViewController()
+        if let localPath = video.localFilePath,
+           FileManager.default.fileExists(atPath: localPath) {
+            let localURL = URL(fileURLWithPath: localPath)
+            return VideoPlayerViewController(url: localURL)
         }
         
-        return VideoPlayerViewController(url: link)
+        if let remoteURL = video.videoFiles?
+            .sorted(by: { ($0.quality ?? "") > ($1.quality ?? "") })
+            .first?.link {
+            return VideoPlayerViewController(url: remoteURL)
+        }
+        
+        let emptyVC = UIViewController()
+        emptyVC.view.backgroundColor = .black
+        let label = UILabel()
+        label.text = "Video unavailable"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.frame = emptyVC.view.bounds
+        emptyVC.view.addSubview(label)
+        return emptyVC
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
