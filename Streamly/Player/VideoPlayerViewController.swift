d0914f579dc3c20fe362bae1c2f0c1ce41c6b02d
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-11-09 18:29:06 -0300

    refactor: use system background color

--- Streamly/Player/VideoPlayerViewController.swift
+++ Streamly/Player/VideoPlayerViewController.swift
@@ -42,7 +42,7 @@ final class VideoPlayerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         setupPlayer()
         setupControls()
         setupProgressObserver()
