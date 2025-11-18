1a46970941bfd6b216bc6929aa72b7d189682e23
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-11-17 23:53:55 -0300

    feat: add AppDelegate and integrate with @main for SwiftUI lifecycle

---
+++ Streamly/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Streamly
+//
+//  Created by Rodrigo Porto on 17/11/25.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
